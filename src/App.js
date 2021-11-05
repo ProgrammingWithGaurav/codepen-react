eee58e3cf3c37b1b0eacf150f3a69b4206814afe
ProgrammingWithGaurav <gaurav2499kumar@gmail.com>
2021-11-05 10:53:08 +0530

    setting html css js pen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Editor from './components/Editor'
 
 function App() {
+  const [html, setHtml] = useState('')
+  const [css, setCss] = useState('')
+  const [js, setJs] = useState('')
   return (
-    'Hello World'
+    <>
+      <div className="pane top-pane">
+        <Editor language='xml' displayName='HTML' value={html} onChange={setHtml} />
+        <Editor language='xml' displayName='css' value={css} onChange={setCss} />
+        <Editor language='xml' displayName='javascript' value={js} onChange={setJs} />
+
+      </div>
+      <div className="pane">
+        <iframe title="output" sandbox="allow-scripts" frameBorder="0" width="100%" height="100%"></iframe>
+      </div>
+    </>
   );
 }
 
