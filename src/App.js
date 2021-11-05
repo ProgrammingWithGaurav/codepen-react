abf250d17a4d11754a8bc8c447848a6b6e3b97bd
ProgrammingWithGaurav <gaurav2499kumar@gmail.com>
2021-11-05 11:53:55 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Editor from './components/Editor'
+import useLocalStorage from './components/hooks/useLocalStorage'
 
 function App() {
-  const [html, setHtml] = useState('')
-  const [css, setCss] = useState('')
-  const [js, setJs] = useState('')
+  const [html, setHtml] = useLocalStorage('html', '')
+  const [css, setCss] = useLocalStorage('css', '')
+  const [js, setJs] = useLocalStorage('js', '')
+
+  const [srcDoc, setSrcDoc] = useState('')
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+          <html>
+              <body>${html}</body>
+              <style>${css}</style>
+              <script>${js}</script>
+          </html>
+        `)
+    }, 250)
+    return () => clearTimeout(timeout)
+  }, [html, css, js])
+
   return (
     <>
       <div className="pane top-pane">
         <Editor language='xml' displayName='HTML' value={html} onChange={setHtml} />
-        <Editor language='xml' displayName='css' value={css} onChange={setCss} />
-        <Editor language='xml' displayName='javascript' value={js} onChange={setJs} />
+        <Editor language='css' displayName='CSS' value={css} onChange={setCss} />
+        <Editor language='javascript' displayName='JS' value={js} onChange={setJs} />
 
       </div>
       <div className="pane">
-        <iframe title="output" sandbox="allow-scripts" frameBorder="0" width="100%" height="100%"></iframe>
+        <iframe srcDoc={srcDoc} title="output" sandbox="allow-scripts" frameBorder="0" width="100%" height="100%"></iframe>
       </div>
     </>
   );
